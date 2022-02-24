48802cb045183e3a207b5652e1cb8fe62db300db
tjweldon <tomd90@gmail.com>
2022-02-24 18:26:45 +0000

    poc of visual mode working!

--- go-brainfuck.go
+++ go-brainfuck.go
@@ -1,24 +1,70 @@
 package main
 
 import (
-	"io"
+	"bytes"
+	"fmt"
+	"github.com/alexflint/go-arg"
+	tm "github.com/buger/goterm"
 	"os"
+	"strings"
+	"time"
 )
+
+const Border = "─ │ ┌ ┐ └ ┘"
 
 type Interpreter struct {
 	iPointer int
 	dPointer int
+	output   []byte
+	input    []byte
+}
+
+type BoxOffset struct {
+	X int
+	Y int
 }
 
 var (
 	instructions []byte
 	memory       [1024]byte
 	err          error
-	interpreter  Interpreter
+	w            int
 )
 
+var args struct {
+	File         string `arg:"-f, --file" help:"The file containing the code"`
+	Instructions string `arg:"-i, --instructions" help:"The brainfuck code"`
+	Visual       bool   `arg:"-v, --visual" help:"Run in visual mode in tmux"`
+	Tick         int    `arg:"-t, --tick" help:"Execute in slow motion, time per instruction in milliseconds" default:"0"`
+	Debug        bool   `arg:"-d, --debug"`
+}
+
+type InterpreterHook func(i Interpreter)
+
+var interpreter = Interpreter{}
+
+func main() {
+	arg.MustParse(&args)
+	instructions = []byte(args.Instructions)
+	if len(instructions) == 0 {
+		instructions, err = os.ReadFile(args.File)
+	}
+	interpreter.input = make([]byte, 32)
+	_, err = os.Stdin.Read(interpreter.input)
+	//fmt.Print(interpreter.input)
+	//fmt.Print(interpreter)
+	interpreter.interpret(
+		args.Tick,
+		[]InterpreterHook{
+			heartbeat(args.Tick),
+			dump,
+		},
+	)
+	_, err = os.Stdout.Write(interpreter.output)
+}
+
 func (interpreter *Interpreter) next() {
-	readBuf := []byte{0}
+	var inByte byte
 	instruction := instructions[interpreter.iPointer]
 	switch instruction {
 	case '>':
@@ -30,13 +76,13 @@ func (interpreter *Interpreter) next() {
 	case '-':
 		memory[interpreter.dPointer]--
 	case '.':
-		_, err = os.Stdout.Write([]byte{memory[interpreter.dPointer]})
+		interpreter.output = append(interpreter.output, memory[interpreter.dPointer])
 	case ',':
-		_, err = os.Stdin.Read(readBuf)
-		if err == io.EOF {
+		if len(interpreter.input[:]) == 0 {
 			os.Exit(0)
 		}
-		memory[interpreter.dPointer] = readBuf[0]
+		inByte, interpreter.input = interpreter.input[0], interpreter.input[1:]
+		memory[interpreter.dPointer] = inByte
 	case '[':
 		if memory[interpreter.dPointer] == 0 {
 			depth := 0
@@ -80,13 +126,94 @@ func (interpreter *Interpreter) next() {
 	interpreter.iPointer++
 }
 
-func (interpreter *Interpreter) interpret() {
+func (interpreter *Interpreter) interpret(tick int, hooks []InterpreterHook) {
 	for interpreter.iPointer < len(instructions) {
+		for _, hook := range hooks {
+			hook(*interpreter)
+		}
 		interpreter.next()
 	}
 }
 
-func main() {
-	instructions = []byte(os.Args[1])
-	interpreter.interpret()
+func (interpreter *Interpreter) String() string {
+	w = tm.Width()
+
+	padding := []byte(strings.Repeat("\x20", w-len(instructions[interpreter.iPointer:])))
+	ticker := append(instructions, padding...)
+	s2 := fmt.Sprintf(
+		"%s\n%s\n%s\n%s\n%s\n",
+		interpreter.input,
+		ticker[interpreter.iPointer:],
+		hexOf(memory[:32]),
+		renderPointer([]byte("___"), []byte("_⇡_"), 32),
+		interpreter.output,
+	)
+	return s2
+}
+
+func hexOf(data []byte) string {
+	result := ""
+	for _, b := range data {
+		result += fmt.Sprintf("%02x ", b)
+	}
+	return result
+}
+
+type Sgr string
+
+const (
+	PtrStyle Sgr = "\x1b[38;5;12m"
+	BgStyle      = "\x1b[1m\x1b[38;5;8m"
+	Default      = "\x1b[0m"
+)
+
+func (s Sgr) String() string {
+	return string(s)
+}
+
+func renderPointer(bgEncoded []byte, ptrEncoded []byte, pRange int) string {
+	increment := len(bgEncoded)
+
+	styledEncodedPtr := fmt.Sprintf("%s%s%s", PtrStyle, string(ptrEncoded), Default+BgStyle)
+
+	renderedBg := bytes.Repeat(bgEncoded, pRange-1)
+	ptrStrOffset := increment * interpreter.dPointer
+
+	return fmt.Sprintf(
+		"%s%s%s%s%s",
+		BgStyle,
+		renderedBg[:ptrStrOffset],
+		styledEncodedPtr,
+		renderedBg[ptrStrOffset:],
+		Default,
+	)
+}
+
+func nothing(_ Interpreter) {
+	return
+}
+
+func heartbeat(tick int) InterpreterHook {
+	if tick == 0 {
+		return nothing
+	}
+
+	return func(_ Interpreter) {
+		time.Sleep(time.Duration(tick) * time.Millisecond)
+	}
+}
+
+func dump(i Interpreter) {
+	fmt.Print(tm.MoveTo(i.String(), 0, 10))
+}
+
+func renderBox(box *tm.Box, boxContent []byte, position BoxOffset) {
+	_, err := fmt.Fprint(box, "   "+string(boxContent))
+	if err != nil {
+		panic(err)
+	}
+	_, err = tm.Print(tm.MoveTo(box.String(), position.X, position.Y))
+	if err != nil {
+		panic(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module tjweldon/go-brainfuck
 
 go 1.17
+
+require (
+	github.com/alexflint/go-arg v1.4.3 // indirect
+	github.com/alexflint/go-scalar v1.1.0 // indirect
+	github.com/buger/goterm v1.0.4 // indirect
+	golang.org/x/sys v0.0.0-20210331175145-43e1dd70ce54 // indirect
+)
